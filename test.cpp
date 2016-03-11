9f64383c83ebe330120ebeb35196aad3e684e171
JWhan <stormhan1205@gmail.com>
2016-03-11 13:15:31 +0800

    Added files via upload

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include "fabonacci.h"
+#include "IsValidSudoku.h"
+#include "BinaryTreePath.h"
+#include "NumberIslands.h"
+#include "BitSwapRequired.h"
+#include "LongestIncreasingContinuousSubsequence.h"
+#include "restoreIp.h"
+
+int main(int argc, char** argv)
+{
+	//fabonacci f;
+	//f.test();
+	//IsValidSudoku i;
+	//i.test();
+	//BinaryTreePath b;
+	//b.test();
+	//NumberIslands n;
+	//n.test();
+	//BitSwapRequired b;
+	//b.test();
+	//LongestIncreasingContinuousSubsequence l;
+	//l.test();
+	restoreIp r;
+	r.restoreIpAddresses("255255111");
+
+	system("pause");
+	return 0;
+}
